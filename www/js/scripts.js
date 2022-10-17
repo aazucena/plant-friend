846cd8a0bb2821e86f8b6fc0d7bf8ac43256dde5
Aldrin Azucena <a.azucena@uleth.ca>
2022-10-17 11:38:20 -0600

    initial push

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -5,3 +5,29 @@ var app = new Framework7({
 });
 var mainView = app.views.create('.view-main')
 
+
+
+
+/**
+* By default:
+* - Plant starts health
+* - Dry out over time
+* - Deplete in nutrients
+*/
+
+/**
+ * Interactions:
+ * - Water it, replenish the plant
+ * - Feed it, nutrient for the plant
+ * - Trim it 
+ */
+
+/**
+ * Conditional Cases:
+ * - If too much water, plant near death
+ * - If too much fert., plant near death
+ * - IF the plant falls into a near death state, heal it by trimming it
+ */
+
+
+console.log()
